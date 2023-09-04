85e303c9dd62f347f794b255d4f7dee3f7763d98
Wali Muhammad Ahmad <99866893+WaliMuhammadAhmad@users.noreply.github.com>
2023-09-04 22:16:39 +0500

    Create Array.cpp

---
+++ Array.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+  const int size = 10;
+  int array[size] = [1,2,3,4,5,6,7,8,9];
+
+  for(int i = 0; i < size; i++) 
+  {
+    cout<<"Array index [" << i's << "] value: " << array[i] ;
+  }
+}
