dbb38c395f87d202259cb26113a9356c0acf58f2
Wali Muhammad Ahmad <99866893+WaliMuhammadAhmad@users.noreply.github.com>
2023-09-14 18:49:49 -0700

    Added Deallocate memory Array.cpp

--- Array.cpp
+++ Array.cpp
@@ -1,13 +1,80 @@
-#include<iostream>
-using namespace std;
+#include <iostream>
 
-int main()
-{
-  const int size = 10;
-  int array[size] = [1,2,3,4,5,6,7,8,9];
+class DynamicArray {
+private:
+    int* array;
+    int size;
+    int capacity;
 
-  for(int i = 0; i < size; i++) 
-  {
-    cout<<"Array index [" << i's << "] value: " << array[i] ;
-  }
+public:
+    // Constructor
+    DynamicArray(int initialCapacity) {
+        capacity = initialCapacity;
+        size = 0;
+        array = new int[capacity];
+    }
+
+    // Destructor
+    ~DynamicArray() {
+        delete[] array;
+    }
+
+    // Function to add an element to the end of the array
+    void pushBack(int element) {
+        if (size == capacity) {
+            // If the array is full, double its capacity
+            capacity *= 2;
+            int* newArray = new int[capacity];
+            for (int i = 0; i < size; i++) {
+                newArray[i] = array[i];
+            }
+            delete[] array;
+            array = newArray;
+        }
+        array[size++] = element;
+    }
+
+    // Function to remove an element from the end of the array
+    void popBack() {
+        if (size > 0) {
+            size--;
+        }
+    }
+
+    // Function to get the element at a specific index
+    int getElementAt(int index) {
+        if (index >= 0 && index < size) {
+            return array[index];
+        }
+        // Return an error value (you can customize this)
+        return -1;
+    }
+
+    // Function to get the current size of the array
+    int getSize() {
+        return size;
+    }
+
+    // Function to get the current capacity of the array
+    int getCapacity() {
+        return capacity;
+    }
+};
+
+int main() {
+    // Create a dynamic array with an initial capacity of 5
+    DynamicArray arr(5);
+
+    // Push elements to the dynamic array
+    for (int i = 1; i <= 10; i++) {
+        arr.pushBack(i);
+    }
+
+    // Print the elements in the array
+    for (int i = 0; i < arr.getSize(); i++) {
+        std::cout << arr.getElementAt(i) << " ";
+    }
+
+    // Deallocate memory when done
+    return 0;
 }
